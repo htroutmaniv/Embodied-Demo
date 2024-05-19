537ba088ff802c229c5645480d61bc9a70e461b6
unknown <htroutmaniv@gmail.com>
2024-05-19 19:13:04 -0400

    script modified, install

--- install.bat
+++ install.bat
@@ -11,7 +11,7 @@ if %errorlevel% neq 0 (
 )
 
 REM Open a new Command Prompt window to run the second part
-start cmd /k scripts\continue_setup.bat
+start cmd /k continue_setup.bat
 
 REM Exit the initial script
 exit
